c9575cb855dc139fddaefc6e070d973f24e46821
weronikawierzbicka <weronikawierzbicka@op.pl>
2017-06-14 23:33:29 +0200

    Wersja ostatecznie ostateczna

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -86,11 +86,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(848, 362);
+            this.ClientSize = new System.Drawing.Size(847, 362);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.bInstrukcja);
             this.Controls.Add(this.bPlansza2);
             this.Controls.Add(this.bPlansza1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
